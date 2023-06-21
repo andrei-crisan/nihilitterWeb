654f380e2c0dbf7c1288f19e838ba354dd2f6ae5
andrei-crisan <andreicrisan@protonmail.com>
2023-06-21 16:45:18 +0300

    Impl: routing

--- NihiliterWeb/src/app/app-routing.module.ts
+++ NihiliterWeb/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './pages/user/user-login/user-login.component';
+import { UserRegisterComponent } from './pages/user/user-register/user-register.component';
+import { NihilListComponent } from './pages/nihil/nihil-list/nihil-list.component';
+import { NihilAddComponent } from './pages/nihil/nihil-add/nihil-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: UserLoginComponent
+  },
+  {
+    path: 'register', component: UserRegisterComponent,
+  },
+  {
+    path: 'nihileets', component: NihilListComponent,
+  },
+  {
+    path: 'post', component: NihilAddComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
